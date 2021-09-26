e4547d07fac10aa1e0d6bb51c59b839522f5e16d
it1906 <githublmao@seznam.cz>
2021-09-26 15:47:26 +0200

    finalni verze

--- znameni.py
+++ znameni.py
@@ -4,29 +4,29 @@ mesic = str(input("Napiste mesic narozeni: "))
 den = int(input("Zadejte den narozeni: "))
 if den < 31:
 	if mesic == 'leden':
-		znameni = 'kozoroh' if (den < 20) else 'vodnar'
+		znameni = 'kozoroh' if (den <= 20) else 'vodnar'
 	elif mesic == 'unor':
-		znameni = 'vodnar' if (den < 20) else 'ryby'
+		znameni = 'vodnar' if (den <= 20) else 'ryby'
 	elif mesic == 'brezen':
-		znameni = 'ryby' if (den < 20) else 'beran'
+		znameni = 'ryby' if (den <= 20) else 'beran'
 	elif mesic == 'duben':
-		znameni = 'beran' if (den < 20) else 'byk'
+		znameni = 'beran' if (den <= 20) else 'byk'
 	elif mesic == 'kveten':
-		znameni = 'byk' if (den < 20) else 'blizenci'
+		znameni = 'byk' if (den <= 20) else 'blizenci'
 	elif mesic == 'cerven':
-		znameni = 'blizenci' if (den < 21) else 'rak'
+		znameni = 'blizenci' if (den <= 21) else 'rak'
 	elif mesic == 'cervenec':
-		znameni = 'rak' if (den < 23) else 'lev'
+		znameni = 'rak' if (den <= 23) else 'lev'
 	elif mesic == 'srpen':
-		znameni = 'lev' if (den < 23) else 'panna'
+		znameni = 'lev' if (den <= 23) else 'panna'
 	elif mesic == 'zari':
-		znameni = 'panna' if (den < 23) else 'vahy'
+		znameni = 'panna' if (den <= 23) else 'vahy'
 	elif mesic == 'rijen':
-		znameni = 'vahy' if (den < 23) else 'stir'
+		znameni = 'vahy' if (den <= 23) else 'stir'
 	elif mesic == 'listopad':
-		znameni = 'stir' if (den < 22) else 'strelec'
+		znameni = 'stir' if (den <= 22) else 'strelec'
 	elif mesic == 'prosinec':
-		znameni = 'strelec' if (den < 21) else 'kozoroh'
+		znameni = 'strelec' if (den <= 21) else 'kozoroh'
 	print("Vase znameni je: ", znameni)
 else:
 	print('Pokud vim, mesic ma nanejvys 31 dni')
